89709e4fe934f1a78a2274107f9e2954fb81cb76
abiao <tyb_178344915@163.com>
2022-04-20 15:27:18 +0800

    first commit

---
+++ src/GitTest.java
@@ -0,0 +1,14 @@
+/**
+ * @ProjectName: git-demo
+ * @Package: PACKAGE_NAME
+ * @ClassName: GitTest
+ * @Author: Yingbiao Tian
+ * @Description:
+ * @Date: 2022-04-20 10:44
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git! Hello world");
+    }
+}
